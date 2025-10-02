47d21cef223c2fc1587834fab742234acc1371c8
xiaofan zhang <601081321@qq.com>
2025-10-02 23:02:10 +0800

    fix display_y1_with_gripper.launch.py

--- y1_ros2/build.sh
+++ y1_ros2/build.sh
@@ -7,7 +7,7 @@
 colcon build --packages-select y1_msg 
 
 # y1 description
-# catkin_make install --only-pkg-with-deps y1_description -j${thread_num}
+colcon build --packages-select y1_description
 
 # y1 arm ros2 driver
 colcon build --packages-select y1_controller
