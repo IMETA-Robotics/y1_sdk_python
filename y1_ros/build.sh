177e6d938c23b22aad5920c550c36bee8fe1294c
ahao626 <15310670619@163.com>
2025-10-25 19:00:06 +0800

    add moveit&gazebo sim control.

--- y1_ros/build.sh
+++ y1_ros/build.sh
@@ -8,8 +8,18 @@ thread_num=$(($(nproc) - 1))
 # y1 arm ros msgs
 catkin_make install --only-pkg-with-deps y1_msg -j${thread_num}
 
-# y1 description
-catkin_make install --only-pkg-with-deps y1_description -j${thread_num}
+# y1 arm gazebo
+catkin_make install --only-pkg-with-deps  y1_gazebo -j${thread_num}
+
+# y1 arm y1_moveit_ctrl
+catkin_make install --only-pkg-with-deps  y1_moveit_ctrl -j${thread_num}
+
+# y1 arm y1_no_gripper_moveit
+catkin_make install --only-pkg-with-deps y1_no_gripper_moveit -j${thread_num}
+
+# y1 arm y1_with_gripper_moveit
+catkin_make install --only-pkg-with-deps  y1_with_gripper_moveit -j${thread_num}
+
 
 # y1 arm ros1 driver
 catkin_make install --only-pkg-with-deps y1_controller -j${thread_num}
